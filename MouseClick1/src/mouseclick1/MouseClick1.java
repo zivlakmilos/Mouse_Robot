2d0c21c1b3e54c3c0a97f7050e34ca6899b5c689
zivlakmilos <zivlakmilos@gmail.com>
2015-05-04 21:36:03 +0200

    Uradjen meni

--- MouseClick1/src/mouseclick1/MouseClick1.java
+++ MouseClick1/src/mouseclick1/MouseClick1.java
@@ -1,7 +1,9 @@
 package mouseclick1;
 
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import net.miginfocom.layout.*;
 
 /**
  *
@@ -12,16 +14,68 @@ import javax.swing.*;
 public class MouseClick1 extends JFrame
 {
     // Visual components
-    JLabel lblX, lblY, lblA;
-    JList lstX, lstY, lstA;
+    private JPanel pnlX, pnlY, pnlA;
+    private JLabel lblX, lblY, lblA;
+    private JTextArea lstX, lstY, lstA;
+    private MenuBar menu;
+    private Menu mMain;
+    private MenuItem miHelp;
     
     // Constructor
     public MouseClick1()
     {
-        // Intitialization
+        /*
+         * Initializetion
+         */
+        
+        // Initialize visual components
+        pnlX = new JPanel();
+        pnlY = new JPanel();
+        pnlA = new JPanel();
         lblX = new JLabel("X");
         lblY = new JLabel("Y");
         lblA = new JLabel("A");
+        lstX = new JTextArea("");
+        lstY = new JTextArea("");
+        lstA = new JTextArea("");
+        
+        menu = new MenuBar();
+        mMain = new Menu();
+        mMain.setLabel("Opcije");
+        miHelp = new MenuItem();
+        miHelp.setLabel("Pomoc");
+        
+        mMain.add(miHelp);
+        menu.add(mMain);
+        
+        // Initialize form
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setMenuBar(menu);
+        
+        this.lookAndFeel();
+        
+        /*
+         * Draw form
+         */
+        
+        /*
+         * Events
+         */
+        
+        // Menu
+        miHelp.addActionListener(new ActionListener()
+        {
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                System.exit(0);
+            }
+        });
+    }
+    
+    private void lookAndFeel()
+    {
     }
     
     //================================= main =================================//
