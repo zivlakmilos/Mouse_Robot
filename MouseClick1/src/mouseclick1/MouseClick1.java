d78adfe363fe4d2cb93662f7ff056c3c1be96da0
zivlakmilos <zivlakmilos@gmail.com>
2015-05-04 20:31:49 +0200

    Moudr_Robot

---
+++ MouseClick1/src/mouseclick1/MouseClick1.java
@@ -0,0 +1,22 @@
+package mouseclick1;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author zi
+ */
+
+
+public class MouseClick1 extends JFrame
+{
+    //================================= main =================================//
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+    }
+    //============================== end of main =============================//
+}
