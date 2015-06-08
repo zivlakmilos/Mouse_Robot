0c67e0777e56d8193f740ca3d8636d1cf8cec988
zivlakmilos <zivlakmilos@gmail.com>
2015-06-09 00:30:48 +0200

    MouseClick4

---
+++ MouseClick4/src/mouseclick4/MouseRobot.java
@@ -0,0 +1,18 @@
+package mouseclick4;
+
+import java.awt.Robot;
+
+/**
+ * 
+ * @author ZI
+ *
+ */
+
+public class MouseRobot extends Thread
+{
+	// Main function of thread
+	@Override
+	public void run()
+	{
+	}
+}
